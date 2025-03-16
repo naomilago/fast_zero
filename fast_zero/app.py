22e029516a260f0c19e46c0444c2e34ca0e7bf80
naomilago <info@naomilago.com>
2025-03-16 12:07:36 -0300

    Add HTML response endpoint and message schema; include banner image

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,29 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK)
+def read_html():
+    style = """
+        <style>
+            h1 {
+                font-size: 32pt;
+                color: teal;
+                text-align: center;
+                margin-top: 50px;
+            }
+        </style>
+    """
+
+    return HTMLResponse(f'{style}<h1>Olá Mundo!</h1>')
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,18 @@ from fast_zero.app import app
 
 
 def test_root_retorna_ola_mundo():
-    client = TestClient(app) # Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') # Act
+    response = client.get('/')  # Act
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_html_retorna_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá Mundo!</h1>' in response.text
